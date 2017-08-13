f45011f7fc3c91de0cb24bef5f377cce77b7fb8e
esparno <elsparno@gmail.com>
2017-08-13 12:33:17 -0600

    dbfirst stored procedures

--- DbFirst/DbFirst/Program.cs
+++ DbFirst/DbFirst/Program.cs
@@ -10,6 +10,13 @@ namespace DbFirst
     {
         static void Main(string[] args)
         {
+            var dbContext = new PlutoDbContext();
+            var courses = dbContext.GetCourses();
+            foreach (var course in courses)
+            {
+                Console.WriteLine(course.Title);
+            }
+            Console.ReadLine();
         }
     }
 }
